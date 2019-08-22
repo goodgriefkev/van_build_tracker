322198494d09f223a2d9d64419fd97d8ec624a10
Kevin Honnoll <khonnoll@gmail.com>
2019-08-22 16:18:42 -0400

    created signin component, rendered to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import SignIn from './components/SignIn.js'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Van Build Tracker</h1>
+        <SignIn />
       </div>
     )
   }
