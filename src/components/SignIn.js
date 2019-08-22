60dba1f951aceed8bbf28d519b355b4cdb1a9d9e
Kevin Honnoll <khonnoll@gmail.com>
2019-08-22 16:24:02 -0400

    created sign in form

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react'
 
 class SignIn extends Component {
+
+  state = {
+    email: '',
+    password: ''
+  }
+
   render() {
     return (
-      <h2>Sign In</h2>
+      <div>
+        <h2>Sign In</h2>
+        <form>
+          <label>
+            <input
+              type='text'
+              name='email'
+              id='email'
+              value={this.state.email}
+              placeholder='email'
+            />
+          </label>
+          <label>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              value={this.state.password}
+              placeholder='password'
+            />
+          </label>
+          <input
+            type='submit'
+            value='Sign In'
+          />
+        </form>
+      </div>
     )
   }
 }
