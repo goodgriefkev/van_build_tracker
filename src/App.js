807cb0b58bffc739b8eacf50d2471a059504c0ca
Kevin Honnoll <khonnoll@gmail.com>
2019-08-23 11:10:02 -0400

    installed and required react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import CreateUser from './components/CreateUser.js'
 import SignIn from './components/SignIn.js'
 import VanBuild from './components/VanBuild.js'
@@ -10,16 +11,37 @@ const baseURL = BASE_URL
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Van Build Tracker</h1>
-        <CreateUser
-          baseURL = { baseURL }
-        />
-        <SignIn
-          baseURL = { baseURL }
-        />
-        <VanBuild />
-      </div>
+      <Router>
+        <div>
+          <h1>Van Build Tracker</h1>
+
+          <Route exact
+            path='/'
+            render={(routeProps) => (
+              <SignIn
+                baseURL = { baseURL }
+              />
+            )}
+          />
+
+          <Route
+            path='/signup'
+            render={(routeProps) => (
+              <CreateUser
+                baseURL = { baseURL }
+              />
+            )}
+          />
+
+          <Route
+            path='/vanbuild'
+            render={(routeProps) => (
+              <VanBuild />
+            )}
+          />
+          
+        </div>
+      </Router>
     )
   }
 }
